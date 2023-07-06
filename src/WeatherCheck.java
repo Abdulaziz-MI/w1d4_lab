79a3df5ed9a4970b8a1d132e6abba6d6589dfa69
Abdulaziz-MI <am.alzaid@outlook.com>
2023-07-06 21:45:40 +0100

    Extension Developed

--- src/WeatherCheck.java
+++ src/WeatherCheck.java
@@ -3,16 +3,30 @@ import java.util.Scanner;
 public class WeatherCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+
+
         System.out.println("Hi, What is the temperature today in degrees Celsius?");
         int temperature = sc.nextInt();
+        String tempAdvice = getTempAdvice(temperature);
 
-/*        The line below is there to stop the terminal from skipping the rain Question.
-          I think if I made classes for both temp and rain it would have solved it */
+
+//  The line below is there to stop the terminal from skipping the rain Question.
         sc.nextLine();
 
 
-        String tempAdvice;
+        System.out.println("Is it currently raining? (y/n)");
+        String isRaining = sc.nextLine();
 
+        boolean isRainingBool = isRaining.equals("y");
+        String rainAdvice =getRainAdvice(isRainingBool);
+
+        System.out.println("Overall, " + tempAdvice + " And " + rainAdvice);
+        }
+
+
+    private static String getTempAdvice(int temperature) {
+        String tempAdvice;
         if (temperature > 10 && temperature <= 15) {
             tempAdvice = "It is very cold, wear a big jacket!";
         } else if (temperature > 15 && temperature <= 20) {
@@ -26,25 +40,19 @@ public class WeatherCheck {
         } else {
             tempAdvice = "It is VERY VERY COLD, find Heating!";
         }
-
-
-        System.out.println("Is it currently raining? (y/n)");
-        String isRaining = sc.nextLine();
-
-        boolean isRainingBool = isRaining.equals("y");
+        return tempAdvice;
+    }
+    private static String getRainAdvice(boolean isRainingBool){
         String rainAdvice;
-
         if (isRainingBool) {
             rainAdvice = "it's raining, get an umbrella";
         } else {
             rainAdvice = "it's not raining, no need for an umbrella";
         }
-
-        System.out.println("Overall, " + tempAdvice + " And " + rainAdvice);
-
-        }
-
-
+        return rainAdvice;
     }
+
+
+}
 
 
